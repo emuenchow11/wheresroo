3c063e7eb70e59bd38fa0407a62847c8998efdd5
emuenchow11 <58281300+emuenchow11@users.noreply.github.com>
2019-12-04 15:42:07 -0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,51 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
 module.exports = {
+    // Webpack configuration goes here
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js'
+    },
+    devtool: 'inline-source-map',
     module: {
         rules: [
+
+            // First Rule
             {
-                test: /\.js$/,
+                test: /\.(js)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                use: ['babel-loader']
             },
-        ],
+
+            // Second Rule
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+
+                    }
+                ]
+            }
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
 };
